5795b2e11a04ddab1196e5471003cf0e32bb8a36
Isaac Haro <74152544+ieharo@users.noreply.github.com>
2020-11-30 08:27:24 -0500

    Add files via upload

---
+++ Pila.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    class Pila
+    {
+        private int[]elemento = new int [20];        
+        private int tope;
+
+        public Pila()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                elemento[i] = 0;
+                tope = -1;
+            }
+        }
+
+        public void Push(int a)
+        {
+            elemento[tope + 1] = a;
+            tope += 1;                
+        }
+        public int Pop() {
+            int auxiliar;
+            auxiliar = elemento[tope];
+            elemento[tope] =0 ;
+            tope -= 1;
+            return auxiliar;
+        }
+        public void ImprimirPila()
+        {
+
+            for (int i = 0; i <= tope; i++)
+            {
+                Console.WriteLine(elemento[i]);
+            }
+            Console.WriteLine("Esto es todo!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pila Mi_Pila = new Pila();
+            Console.WriteLine("Cuántos datos desea ingresar=");
+            int x;
+            x = int.Parse(Console.ReadLine());
+            for (int i=0;i<x;i++) {
+                Console.WriteLine("Ingrese el número {0}",i+1);
+                Mi_Pila.Push(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine("Cuántos datos desea Retirar");;
+            x = int.Parse(Console.ReadLine());
+            for (int i = 0; i < x; i++)
+            {
+                Mi_Pila.Pop();
+            }
+            Mi_Pila.ImprimirPila();
+        }
+    }
+}
